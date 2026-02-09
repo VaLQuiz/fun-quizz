d3cfb8a841ac23be12da3e36b79186ed64d037ed
VaLQuiz <mohx59400@gmail.com>
2026-02-09 16:59:35 +0100

    Add files via upload

---
+++ Fun_Quizz_Android_Studio_Project/app/src/main/java/com/funquizz/app/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.funquizz.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl("https://example.com") // Remplacer par ton futur site
+    }
+}
